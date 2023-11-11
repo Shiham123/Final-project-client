e5f457ae0ef4a377a117666951d2ebbb0c89275e
Shiham123 <s36651786@gmail.com>
2023-11-11 09:56:15 +0600

    color update based on state and add feature section

--- src/Section/Featured/FeatureSection.jsx
+++ src/Section/Featured/FeatureSection.jsx
@@ -1,26 +1,39 @@
+import { useEffect, useState } from 'react';
 import SectionTitle from '../../SubSection/SectionTitle';
 import featureBgImg from '../../assets/home/featured.jpg';
 
 const FeatureSection = () => {
+  const [isCheck, setIsCheck] = useState(true);
+
+  useEffect(() => {
+    setIsCheck(true);
+  }, []);
+
   return (
     <section className="my-[10rem]">
-      <SectionTitle heading="Featured item" subHeading="check it out" />
       <div className="relative">
         <img src={featureBgImg} className="blur-sm brightness-50" alt="" />
-        <div className="flex justify-center items-center py-8 px-[10rem] gap-8 absolute top-[25rem] text-fontColorOne">
-          <div>
-            <img src={featureBgImg} className="rounded-lg" alt="" />
-          </div>
-          <div>
-            <p>Aug 20, 2029</p>
-            <p className="uppercase">WHERE CAN I GET SOME?</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-              voluptate facere, deserunt dolores maiores quod nobis quas quasi.
-              Eaque repellat recusandae ad laudantium tempore consequatur
-              consequuntur omnis ullam maxime tenetur
-            </p>
-            <button>Open now</button>
+        <div className="absolute top-[10rem]">
+          <SectionTitle
+            heading="Featured item"
+            subHeading="check it out"
+            isCheck={isCheck}
+          />
+          <div className="flex justify-center items-center py-8 px-[10rem] gap-8 text-fontColorOne">
+            <div>
+              <img src={featureBgImg} className="rounded-lg" alt="" />
+            </div>
+            <div>
+              <p>Aug 20, 2029</p>
+              <p className="uppercase">WHERE CAN I GET SOME?</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                voluptate facere, deserunt dolores maiores quod nobis quas
+                quasi. Eaque repellat recusandae ad laudantium tempore
+                consequatur consequuntur omnis ullam maxime tenetur
+              </p>
+              <button className="btn">Open now</button>
+            </div>
           </div>
         </div>
       </div>
--- src/SubSection/SectionTitle.jsx
+++ src/SubSection/SectionTitle.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 
 const SectionTitle = (props) => {
-  const { heading, subHeading } = props;
+  const { heading, subHeading, isCheck } = props;
   return (
     <div className="flex flex-col justify-center items-center gap-8 my-20">
       <h2 className="text-xl font-inter uppercase text-subTitleColor">
         {subHeading}
       </h2>
-      <h1 className="text-footerBgColorThree uppercase font-inter text-2xl md:text-3xl lg:text-4xl border-t-4 border-borderColorOne py-4 border-b-4 w-2/3 md:w-2/4 lg:w-1/4 text-center">
+      <h1
+        className={
+          isCheck
+            ? 'text-fontColorOne uppercase font-inter text-2xl md:text-3xl lg:text-4xl border-t-4 border-borderColorOne py-4 border-b-4 w-2/3 md:w-2/4 lg:w-1/4 text-center'
+            : 'text-footerBgColorThree uppercase font-inter text-2xl md:text-3xl lg:text-4xl border-t-4 border-borderColorOne py-4 border-b-4 w-2/3 md:w-2/4 lg:w-1/4 text-center'
+        }
+      >
         {heading}
       </h1>
     </div>
@@ -17,6 +23,7 @@ const SectionTitle = (props) => {
 SectionTitle.propTypes = {
   heading: PropTypes.string.isRequired,
   subHeading: PropTypes.string,
+  isCheck: PropTypes.string,
 };
 
 export default SectionTitle;
