ab74af6360c645a19c55382ef623978a083895af
Shiham123 <s36651786@gmail.com>
2023-11-12 15:35:14 +0600

    tanstack query and axiox install

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import './main.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Router.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
